b8be5e2147a4be8b268621160f4309d385eba3af
Dharlan Oliveira <almeidao@tcu.gov.br>
2016-11-21 17:18:57 -0200

    |

--- components/select/off-click.js
+++ components/select/off-click.js
@@ -5,11 +5,20 @@ var OffClickDirective = (function () {
     }
     /* tslint:enable */
     OffClickDirective.prototype.onClick = function ($event) {
-        $event.stopPropagation();
+        //$event.stopPropagation();
+        var _this = this;
+        // Permite propagar o evento de clique para fechar outras eventuais combos que estejam abertas.
+        // Para não fechar a combo atual, remove temporariamente o handler responsável por fechar a combo atual a partir de um evento de clique.
+        document.removeEventListener('click', this.offClickHandler);
+        setTimeout(function () {
+            document.addEventListener('click', _this.offClickHandler);
+        }, 0);
     };
     OffClickDirective.prototype.ngOnInit = function () {
         var _this = this;
-        setTimeout(function () { document.addEventListener('click', _this.offClickHandler); }, 0);
+        setTimeout(function () {
+            document.addEventListener('click', _this.offClickHandler);
+        }, 0);
     };
     OffClickDirective.prototype.ngOnDestroy = function () {
         document.removeEventListener('click', this.offClickHandler);
--- components/select/off-click.ts
+++ components/select/off-click.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, Input, OnInit, OnDestroy } from '@angular/core';
+import {Directive, HostListener, Input, OnInit, OnDestroy} from '@angular/core';
 
 @Directive({
   selector: '[offClick]'
@@ -8,12 +8,22 @@ export class OffClickDirective implements OnInit, OnDestroy {
   /* tslint:disable */
   @Input('offClick') public offClickHandler: any;
   /* tslint:enable */
-  @HostListener('click', ['$event']) public onClick($event: MouseEvent): void {
-    $event.stopPropagation();
+  @HostListener('click', ['$event'])
+  public onClick($event: MouseEvent): void {
+    //$event.stopPropagation();
+
+    // Permite propagar o evento de clique para fechar outras eventuais combos que estejam abertas.
+    // Para não fechar a combo atual, remove temporariamente o handler responsável por fechar a combo atual a partir de um evento de clique.
+    document.removeEventListener('click', this.offClickHandler);
+    setTimeout(() => {
+      document.addEventListener('click', this.offClickHandler);
+    }, 0);
   }
 
   public ngOnInit(): any {
-    setTimeout(() => {document.addEventListener('click', this.offClickHandler);}, 0);
+    setTimeout(() => {
+      document.addEventListener('click', this.offClickHandler);
+    }, 0);
   }
 
   public ngOnDestroy(): any {
